b9d59ee79f7daa34369ffe3f8c7d931b0d302fdc
ken.wu <ken.wu@ruckuswireless.com>
2015-12-08 11:34:51 +0800

    Fill metadata to packages table

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
 import "fmt"
 import "os"
 import "errors"
@@ -41,6 +46,7 @@ func parseRPMFiles(ctx context.Context, in <-chan string) <-chan *packageInfo {
 			default:
 				info, err := ts.parsePackageInfo(path)
 				if err != nil {
+					log.Println(err.Error())
 					continue
 				}
 
@@ -95,6 +101,74 @@ func findRPMFiles(ctx context.Context, dir string) <-chan string {
 	return files
 }
 
+func repeatStr(n uint32, x string) []string {
+	v := make([]string, n)
+	for i, _ := range v {
+		v[i] = x
+	}
+
+	return v
+}
+
+func genMetadata(ctx context.Context, c <-chan *packageInfo) error {
+	db, err := sql.Open("sqlite3", "/tmp/primary.sqlite")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	if err = initPrimaryDB(db); err != nil {
+		return err
+	}
+
+	placeHolders := strings.Join(repeatStr(25, "?"), ",")
+
+	stmt, err := db.Prepare(fmt.Sprintf("INSERT INTO packages (pkgId, name, arch, version, epoch, release, summary, description, url, time_file, time_build, rpm_license, rpm_vendor, rpm_group, rpm_buildhost, rpm_sourcerpm, rpm_header_start, rpm_header_end, rpm_packager, size_package, size_installed, size_archive, location_href, location_base, checksum_type) values (%s)", placeHolders))
+	if err != nil {
+		return err
+	}
+
+	for p := range c {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+		}
+
+		_, err = stmt.Exec(
+			p.checksum,
+			p.rpmName,
+			p.rpmArch,
+			p.rpmVersion,
+			p.rpmEpoch,
+			p.rpmRelease,
+			p.rpmSummary,
+			p.rpmDescription,
+			p.rpmUrl,
+			p.fileTime,
+			p.rpmBuildTime,
+			p.rpmLicense,
+			p.rpmVendor,
+			p.rpmGroup,
+			p.rpmBuildHost,
+			p.rpmSourceRpm,
+			p.headerStart,
+			p.headerEnd,
+			p.rpmPackager,
+			p.fileSize,
+			p.rpmInstallSize,
+			p.rpmArchiveSize,
+			"", // location_href
+			"", // location_base
+			p.checksumType,
+		)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		panic("We accept exactly one argument which is a directory.")
@@ -104,7 +178,12 @@ func main() {
 	files := findRPMFiles(ctx, os.Args[1])
 	out := parseRPMFiles(ctx, files)
 
-	for info := range out {
-		log.Printf("%s:%s, <%d,%d>", info.path, info.checksum, info.headerStart, info.headerEnd)
+	err := genMetadata(ctx, out)
+	if err != nil {
+		panic(err)
 	}
+
+	// for info := range out {
+	// 	log.Printf("%s:%+v", info.path, info)
+	// }
 }
